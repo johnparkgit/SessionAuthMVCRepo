45e5c49f4df96a3cba0f6307d74ba103a4d8ab55
john <jpark@cdsglobal.ca>
2018-10-26 12:32:33 -0400

    init

--- SessionAuthMVC/Controllers/HomeController.cs
+++ SessionAuthMVC/Controllers/HomeController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SessionAuthMVC.Filters;
 namespace SessionAuthMVC.Controllers
 {
+    [UserAuthenticationFilter]
     public class HomeController : Controller
     {
         public ActionResult Index()
